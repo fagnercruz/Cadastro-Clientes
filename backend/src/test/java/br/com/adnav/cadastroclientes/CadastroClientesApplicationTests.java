884cba83d60aac938b9ada85c82ff1ad9024c3f9
Fagner Cunha da Cruz <fagnerccruz@gmail.com>
2023-08-15 14:18:37 -0300

    upload base spring project

---
+++ backend/src/test/java/br/com/adnav/cadastroclientes/CadastroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adnav.cadastroclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
