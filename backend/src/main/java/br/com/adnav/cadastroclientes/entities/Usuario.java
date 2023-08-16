ef8b57d89f7f7199f7c833ff62d86545a940a42d
Fagner Cunha da Cruz <fagnerccruz@gmail.com>
2023-08-16 18:40:57 -0300

    FInalização das entidades principais e seed do banco

--- backend/src/main/java/br/com/adnav/cadastroclientes/entities/Usuario.java
+++ backend/src/main/java/br/com/adnav/cadastroclientes/entities/Usuario.java
@@ -1,18 +1,122 @@
 package br.com.adnav.Cadastroclientes.entities;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private int idade;
+	private Long cpf;
+	private Long identidade;
+	private String expedidor;
+	private String expedidorUf;
+	private String telefone; 
 	
-	/** Construtor sem argumentos */
 	public Usuario() {}
-	
-	// TODO Fazer construtor com argumentos
-	
-	// TODO Fazer getters e setters
-	
-	// TODO Fazer equals e Hashcode
-	
+
+	public Usuario(Long id, String nome, int idade, Long cpf, Long identidade, String expedidor, String expedidorUf,
+			String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+		this.cpf = cpf;
+		this.identidade = identidade;
+		this.expedidor = expedidor;
+		this.expedidorUf = expedidorUf;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getIdentidade() {
+		return identidade;
+	}
+
+	public void setIdentidade(Long identidade) {
+		this.identidade = identidade;
+	}
+
+	public String getExpedidor() {
+		return expedidor;
+	}
+
+	public void setExpedidor(String expedidor) {
+		this.expedidor = expedidor;
+	}
+
+	public String getExpedidorUf() {
+		return expedidorUf;
+	}
+
+	public void setExpedidorUf(String expedidorUf) {
+		this.expedidorUf = expedidorUf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
 }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO usuario (nome, idade, cpf, identidade, expedidor, expedidor_uf, telefone) VALUES ('Fagner Cunha da Cruz', 41, 12345678900L , 12345678L, 'SSP', 'PB', '(83) 98888-8888');
+INSERT INTO usuario (nome, idade, cpf, identidade, expedidor, expedidor_uf, telefone) VALUES ('Maria Aparecida', 37, 12345678900L , 12345678L, 'SSP', 'PB', '(83) 98888-8888');
+INSERT INTO usuario (nome, idade, cpf, identidade, expedidor, expedidor_uf, telefone) VALUES ('Vanderlaine da Silva Cunha', 30, 12345678900L , 12345678L, 'SSP', 'PB', '(83) 98888-8888');
+INSERT INTO usuario (nome, idade, cpf, identidade, expedidor, expedidor_uf, telefone) VALUES ('Thomás Kitubaína da Serra', 59, 12345678900L , 12345678L, 'SSP', 'PB', '(83) 98888-8888');
+
+INSERT INTO endereco (cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('58085674', 'Rua Capitão Jair Bolsonaro', 457, 'Apt 702-A', 'Ponta de Campina', 'Cabedelo', 'PB');
+INSERT INTO endereco (cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('58085674', 'Rua Presidiário Lula da Silva', 171, '', 'Presídio do Róger', 'João Pessoa', 'PB');
+INSERT INTO endereco (cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('58085674', 'Rua Teste Maroto', 159, 'Casa 06', 'Muçumagro', 'João Pessoa', 'PB');
+INSERT INTO endereco (cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('58085674', 'Rua dos excluídos', 17, 'Apt 102', 'Jardim Planalto', 'João Pessoa', 'PB');
