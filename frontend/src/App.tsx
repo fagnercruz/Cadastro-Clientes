0ead5fa7f38c2fdbd951295e34e879c1a4a9ded3
Fagner Cruz <fagner@sti.ufpb.br>
2023-08-22 10:11:32 -0300

    Init frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Formcard from "./Formcard";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>CADASTRO DE CLIENTES</h2>
+      <Formcard />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Formcard/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Formcard = () => {
+  return (
+    <div className="formcard">
+      <form>
+        <input type="text" id="inputNome" placeholder="Digite seu nome" />
+      </form>
+    </div>
+  );
+};
+
+export default Formcard;
