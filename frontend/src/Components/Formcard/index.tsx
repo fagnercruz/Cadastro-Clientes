928e40a753386f4f5a6f946bf40fdce2867a9b4c
Fagner Cruz <fagner@sti.ufpb.br>
2023-08-25 14:17:09 -0300

    Adição de novos campos imput e adicao do react-hook-form e bootstrap

--- frontend/src/Components/Formcard/index.tsx
+++ frontend/src/Components/Formcard/index.tsx
@@ -5,6 +5,8 @@ const Formcard = () => {
     <div className="formcard">
       <form>
         <input type="text" id="inputNome" placeholder="Digite seu nome" />
+        <input type="text" id="inputCep" placeholder="CEP" />
+        <input type="text" id="inputEndereco" placeholder="Endereço" />
         <button>Enviar</button>
       </form>
     </div>
